a0e9b3bd8a732de8bb478878726229e671c978fe
Amie Wilt <amie.wilt@gmail.com>
2015-05-25 10:45:11 -0400

    got tests 1 and 2 passing

--- green_eggs_and_ham.rb
+++ green_eggs_and_ham.rb
@@ -3,4 +3,24 @@ class GreenEggsAndHam
   def initialize
     @text = File.read('green_eggs_and_ham.txt')
   end
+
+  def book
+    @book
+  end
+
+  def word_count
+    @text.split.length
+  end
+
+  def sorted_unique_words
+    @text.downcase.gsub(/[^a-z-]/, ' ').split.uniq.sort
+  end
+
+  def number_of_words_shorter_than(x)
+    @wordsarray = @text.downcase.gsub(/[^a-z-]/, ' ').split.uniq.sort
+    @newarray = @wordsarray.reject { |i| i.length > x }
+    @newarray.length
+  end
 end
+
+puts @newarray.lengthgit
