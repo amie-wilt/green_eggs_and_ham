4d6e37431fe27561dc949bacaff4b0d1731c1103
Amie Wilt <amie.wilt@gmail.com>
2015-05-26 09:20:17 -0400

    defined words and used to count and sort unique words

--- green_eggs_and_ham.rb
+++ green_eggs_and_ham.rb
@@ -4,16 +4,20 @@ class GreenEggsAndHam
     @text = File.read('green_eggs_and_ham.txt')
   end
 
+  def words
+    @text.split.map { |word| word.downcase.gsub(/[^a-z-]/, '') }
+  end
+
   def book
     @book
   end
 
   def word_count
-    @text.split.length
+    words.length
   end
 
   def sorted_unique_words
-    @text.downcase.gsub(/[^a-z-]/, ' ').split.uniq.sort
+    words.uniq.sort
   end
 
   def number_of_words_shorter_than(x)
